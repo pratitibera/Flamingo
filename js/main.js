12ce5a99ebd6e0077854405c694ddd43d82a4d96
Pratiti Bera <remabimal0801@gmail.com>
2022-03-28 19:39:25 +0530

    Removed owl carousel dependencies

--- js/main.js
+++ js/main.js
@@ -10,21 +10,3 @@ menuBtn.addEventListener('click', () => {
         menuOpen = false;
     }
 });
-
-$('#header_carousel').owlCarousel({
-    loop: true,
-    autoplay: true,
-    autoPlaySpeed: 1000,
-    autoplayHoverPause: true,
-    dots: false,
-    nav: true,
-    navText: [$('.owl-navigation-header_carousel .owl-nav-prev'), $('.owl-navigation-header_carousel .owl-nav-next')],
-    responsive: {
-        0: {
-            items: 1
-        },
-        960: {
-            items: 3.15
-        }
-    }
-});
